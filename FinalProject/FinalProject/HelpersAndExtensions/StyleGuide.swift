6cd963a2b3fe715b7a0d7363cb33c92ce31d2a28
Clarissa Vinciguerra <clarissavinciguerra@Luiss-MacBook-Pro.local>
2020-11-27 21:14:49 -0800

    Style guide, fonts and initial color suggestions added

---
+++ FinalProject/FinalProject/HelpersAndExtensions/StyleGuide.swift
@@ -0,0 +1,46 @@
+//
+//  StyleGuide.swift
+//  FinalProject
+//
+//  Created by Clarissa Vinciguerra on 11/25/20.
+//
+
+import UIKit
+
+struct FontNames {
+    static let sourceSansProBold = "SourceSansPro-Bold"
+    static let sourceSansProRegular = "SourceSansPro-Regular"
+    static let sourceSansProLight = "SourceSansPro-Light"
+    static let sourceSansProExtraLight = "SourceSansPro-ExtraLight"
+    
+}
+
+extension UIColor {
+    static let darkerGreen = UIColor(named: "darkerGreen")!
+    static let grayscale = UIColor(named: "grayscale")!
+    static let mainGreen = UIColor(named: "mainGreen")!
+    static let softBlack = UIColor(named: "softBlack")!
+    static let goldAccent = UIColor(named: "goldAccent")!
+    static let lightYellowAccent = UIColor(named: "lightYellow")!
+    static let spaceBlack = UIColor(named: "spaceBlack")!
+    static let whiteSmoke = UIColor(named: "whiteSmoke")!
+    static let subtleText = UIColor(named: "subtleText")!
+    static let borderHighlight = UIColor(named: "borderHighlight")!
+}
+
+extension UIView {
+
+    func addCornerRadius(radius: CGFloat = 4) {
+        self.layer.cornerRadius = radius
+    }
+    
+    func addAccentBorder(width: CGFloat = 3, color: UIColor = .borderHighlight) {
+        self.layer.borderWidth = width
+        self.layer.borderColor = color.cgColor
+    }
+    
+    func rotate(by radians: CGFloat = (-CGFloat.pi / 2)) {
+        self.transform = CGAffineTransform(rotationAngle: radians)
+    }
+}
+
