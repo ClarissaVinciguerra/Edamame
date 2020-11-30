fa3abd76e998925dba0c5ada08a615b787cc01e5
Clarissa Vinciguerra <clarissavinciguerra@Luiss-MacBook-Pro.local>
2020-11-30 10:09:01 -0800

    Adds button UI for friend status

--- FinalProject/FinalProject/Controllers/ViewControllers/ProfileViewController.swift
+++ FinalProject/FinalProject/Controllers/ViewControllers/ProfileViewController.swift
@@ -23,6 +23,7 @@ class ProfileViewController: UIViewController {
     // MARK: - Properties
     var viewsLaidOut = false
     var profileImages: [UIImage] = []
+    var otherUser: User?
     
     // MARK: - Lifecyle Functions
     override func viewDidLoad() {
@@ -61,15 +62,15 @@ class ProfileViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func addAcceptRevokeButtonTapped(_ sender: Any) {
-        
+        checkFriendStatus()
     }
     
     @IBAction func declineButtonTapped(_ sender: Any) {
-        
+        declineFriendRequest()
     }
     
     @IBAction func blockButtonTapped(_ sender: Any) {
-        
+        blockUser()
     }
     
     @IBAction func reportButtonTapped(_ sender: Any) {
@@ -80,6 +81,131 @@ class ProfileViewController: UIViewController {
     
     // MARK: - Class Methods
     
+    func checkFriendStatus() {
+        guard let otherUser = otherUser, let currentUser = UserController.shared.currentUser else { return }
+
+        if currentUser.sentRequests.contains(otherUser.uuid) {
+            removeSentRequestOf(currentUser, andOtherUser: otherUser)
+            
+            updateViews()
+            
+        } else if currentUser.pendingRequests.contains(otherUser.uuid) {
+            removeSentRequestOf(otherUser, andOtherUser: currentUser)
+            
+            currentUser.friends.append(otherUser.uuid)
+            otherUser.friends.append(currentUser.uuid)
+            
+            update(currentUser)
+            update(otherUser)
+            
+            updateViews()
+            
+        } else {
+            currentUser.sentRequests.append(otherUser.uuid)
+            otherUser.pendingRequests.append(currentUser.uuid)
+            
+            update(currentUser)
+            update(otherUser)
+            
+            updateViews()
+        }
+    }
+    
+    private func update(_ user: User) {
+        UserController.shared.updateUserBy(user) { (result) in
+            switch result {
+            case .success(_):
+                DispatchQueue.main.async {
+                    print("User Updated Successfully")
+                }
+            case .failure(let error):
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+    }
+    
+    private func removeSentRequestOf(_ user: User, andOtherUser: User) {
+        
+        UserController.shared.removeFromSentRequestsOf(user, andOtherUser: andOtherUser) { (result) in
+            switch result {
+            case .success(_):
+                DispatchQueue.main.async {
+                    print("Sent request successfully revoked🏅 🔥 Go check Firebase! 🔥")
+                }
+            case .failure(let error):
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+    }
+    
+    func declineFriendRequest() {
+        guard let otherUser = otherUser, let currentUser = UserController.shared.currentUser else { return }
+        
+        blockUser()
+        removeSentRequestOf(otherUser, andOtherUser: currentUser)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func blockUser() {
+        guard let otherUser = otherUser, let currentUser = UserController.shared.currentUser else { return }
+        // change name to uuid
+        currentUser.blockedArray.append(otherUser.uuid)
+        UserController.shared.updateUserBy(currentUser) { (result) in
+            switch result {
+            case .success(_):
+                DispatchQueue.main.async {
+                    print("OtherUser UUID has been successfully appended to currentUsers blocked array.")
+                }
+            case .failure(let error):
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+    }
+    
+    // MARK: - UpdateViews
+    func updateViews() {
+        guard let otherUser = otherUser, let currentUser = UserController.shared.currentUser else { return }
+        
+        nameAndAgeLabel.text = otherUser.name
+        declineButton.alpha = 0
+        addAcceptRevokeButton.alpha = 1
+       
+        UserController.shared.fetchUserByUUID(currentUser.uuid) { (result) in
+            switch result {
+            case .success(let user):
+                DispatchQueue.main.async { [self] in
+                    UserController.shared.currentUser = user
+
+                    // this ends up getting called before the friend request is revoked on the other queue
+                    if currentUser.sentRequests.contains(otherUser.uuid) {
+                        
+                        self.addAcceptRevokeButton.setTitle("Revoke Sent Request", for: .normal)
+                        
+                    } else if currentUser.pendingRequests.contains(otherUser.uuid) {
+                        
+                        self.addAcceptRevokeButton.setTitle("Approve Request", for: .normal)
+                        self.declineButton.alpha = 1
+                        self.declineButton.setTitle("Decline Request", for: .normal)
+                        
+                    } else {
+                        
+                        self.addAcceptRevokeButton.setTitle("Request Friend", for: .normal)
+                    }
+
+                    if currentUser.blockedArray.contains(otherUser.uuid) {
+                        self.addAcceptRevokeButton.alpha = 0
+                        self.blockButton.alpha = 1
+                        self.blockButton.setTitle("Unblock", for: .normal)
+                    } else {
+                        self.blockButton.setTitle("Block", for: .normal)
+                    }
+                }
+            case .failure(let error):
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+        
+    }
 }
 
 //MARK: - Extensions
