6aa22de55b97a24573e82989ff71a33e3d942558
Clarissa Vinciguerra <clarissavinciguerra@Luiss-MacBook-Pro.local>
2021-01-01 12:07:46 -0800

    creates friendship model and model controller with create and fetch functions

--- FinalProject/FinalProject/HelpersAndExtensions/FriendshipError.swift
+++ FinalProject/FinalProject/HelpersAndExtensions/FriendshipError.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+enum FriendshipError: LocalizedError {
+    case firebaseError(Error)
+    case couldNotUnwrap
+    case noExistingUser
+    case errorConvertingImage
+    case couldNotRemove
+    case errorDeletingFriendship(Error)
+    
+    var errorDescription: String {
+        switch self {
+        case .firebaseError(let error):
+            return "There was an error retrieving friendship data from Firebase ===> \(error.localizedDescription)"
+        case .couldNotUnwrap:
+            return "Could not unwrap user data."
+        case .noExistingUser:
+            return "User not found."
+        case .errorConvertingImage:
+            return "Image could not be converted into data to store in Firebase."
+        case.couldNotRemove:
+            return "Unable to delete user account"
+        case .errorDeletingFriendship(let error):
+            return "There was an error trying to delete a user from authentication ===> \(error.localizedDescription)"
+        }
+    }
+}
--- FinalProject/FinalProject/Models/Friendship.swift
+++ FinalProject/FinalProject/Models/Friendship.swift
@@ -6,24 +6,39 @@
 //
 
 import Foundation
+import Firebase
 
-struct FriendRequestKeys {
-    static let typeKey = "Friend"
-    static let userRefKey = "userRef"
-    static let sentKey = "sent"
+struct FriendshipStrings {
+    static let fromUUIDKey = "sentFrom"
+    static let toUUIDKey = "sentTo"
     static let acceptedKey = "accepted"
-    static let siblingRefKey = "siblingRef"
+    static let terminatedKey = "terminated"
 }
 
-class Friend {
+class Friendship {
+    let uuid: String
     var fromUUID: String
     var toUUID: String
-    var status: Status
+    var accepted: Bool
+    var terminated: Bool
     
+    init(uuid: String = UUID().uuidString, fromUUID: String, toUUID: String, accepted: Bool = false, terminated: Bool = false) {
+        self.uuid = uuid
+        self.fromUUID = fromUUID
+        self.toUUID = toUUID
+        self.accepted = accepted
+        self.terminated = terminated
+    }
+    
+    convenience init?(document: DocumentSnapshot) {
+        guard let fromUUID = document[FriendshipStrings.fromUUIDKey] as? String,
+              let toUUID = document[FriendshipStrings.toUUIDKey] as? String,
+              let accepted = document[FriendshipStrings.acceptedKey] as? Bool,
+              let terminated = document[FriendshipStrings.terminatedKey] as? Bool
+              else { return nil }
+       
+        
+        self.init(uuid: document.documentID, fromUUID: fromUUID, toUUID: toUUID, accepted: accepted, terminated: terminated)
+    }
 }
-enum Status {
-    case waiting
-    case accepted
-    case declined
-    case removeFriend
-}
+
